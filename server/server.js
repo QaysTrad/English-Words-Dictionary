ccd51fcd84c7dfdd3ecb953816df29dd553766ae
QaysTrad <qaystrad@gamil.com>
2018-10-23 18:22:00 +0300

    post for the dectionary

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path'); 
 const app = express(); 
+const bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, '../react-client/build')));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-
+app.post('/defWord', (req, res) => {
+    console.log(req.body)
+})
 // all the http methods, and the handler functions in the handler file.
 app.get('/*', (req, res) => {
     res.sendFile(path.resolve(path.join(__dirname, '../react-client/build/index.html')))
