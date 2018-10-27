30883cc4a0cb5c955df7f07f236b184b97dadfc3
QaysTrad <qaystrad@gamil.com>
2018-10-27 13:27:33 +0200

    edit the style and css for the data

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -8,19 +8,30 @@ class App extends Component {
     super(props)
 
     this.state = {
-      theWord: ''
+      theWord: '',
+      data: [],
     }
   }
   sendWord = (word) => {
-    console.log('me', this.state.theWord)
+    var meaning = []
+    var lexicalCategory = []
     axios.post('/defWord', { word: word })
-      .then(() => {
-        console.log('done');
+      .then((data) => {
+        for (var i = 0; i < data.data.results.length; i++) {
+          meaning.push(data.data.results[i].lexicalEntries);
+        }
+        for (var j = 0; j < meaning.length; j++) {
+          for (var s = 0; s < meaning[j].length; s++) {
+            lexicalCategory.push(meaning[j][s])
+          }
+        }
+        this.setState({ data: lexicalCategory })
       })
       .catch((err) => {
         throw err;
       })
   }
+
   onChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value
@@ -28,30 +39,69 @@ class App extends Component {
   };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h2>Dictionary</h2>
-          <TextField
-            id="theWord"
-            label="Word"
-            name='theWord'
-            onChange={this.onChange}
-            margin="normal"
-            variant="outlined"
-            fullWidth
-            className="text"
-          />
-          <br />
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => this.sendWord(this.state.theWord)}
-          >Defintion
+      <div className="bod">
+        <div className="App">
+          <header className="App-header">
+            <h2>Dictionary</h2>
+            <TextField
+              id="theWord"
+              label="Word"
+              name='theWord'
+              onChange={this.onChange}
+              margin="normal"
+              variant="outlined"
+              fullWidth
+              className="text"
+            />
+            <br />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => this.sendWord(this.state.theWord)}
+            >Defintion
           </Button>
-          <div className="Word">
+            <div className="Word">
+              {this.state.data.map(mean => {
+                console.log(mean)
+                return (
+                  <div>
+                    <h3>Category: {mean.lexicalCategory}
+                    </h3>
+                    {mean.entries.map(s => {
+                      return (
+                        <div>
+                          {s.senses.map(v => {
 
-          </div>
-        </header>
+                            if (v.definitions !== undefined) {
+                              return (
+                                <div>
+                                  <h4>definition : {v.short_definitions[0]}</h4>
+                                </div>
+                              )
+                            }
+                          })}
+                          {s.senses.map(ex => {
+                            if (ex.examples !== undefined) {
+                              for (var x = 0; x < ex.examples.length; x++) {
+                                return (
+                                  <div>
+                                    <h4>example : {ex.examples[x].text}</h4>
+                                  </div>
+                                )
+                              }
+                            }
+                          })}
+                        </div>
+                      )
+                    })
+                    }
+                  </div>
+                )
+              })
+              }
+            </div>
+          </header>
+        </div>
       </div>
     );
   }
